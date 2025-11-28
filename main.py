cf92d55655662fa6de4af9e29e19839369da519a
alex13132000 <bludniku1000@gmail.com>
2025-11-28 19:46:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pygame
 import player
 
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((420, 720))
 clock = pygame.time.Clock()
 running = True
 
