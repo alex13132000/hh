e0157d28d7790f53248a591153e4a7bbe3c885d8
Al Nikolov <roottoorfieuorg@gmail.com>
2026-01-23 21:55:58 +0200

    Complete the background scrolling

--- background.py
+++ background.py
@@ -2,38 +2,32 @@ import pygame
 
 
 BG_FILE = 'assets/images/BG/BG_1.png'
+
+
 class Background:
-
-
-
-    def __init__(self, image_path=BG_FILE, speed=2):
+    def __init__(self, screen, image_path=BG_FILE, speed=2):
+        self.screen = screen
         self.speed = speed
         self.y = 0
-        self.image = pygame.image.load(image_path)
-        self.height = self.image.get_height()
-        # self.image = pygame.image.load(image_path)
-        
-        flip_image = pygame.transform.flip(self.image, True, False)
-        bg_size = self.image.get_size()
-        bg_fin = (bg_size[0], bg_size[1] * 2)
-        self.big_bg = pygame.Surface(bg_fin)
-        self.big_bg.blit(self.image, (0, 0))
-        self.big_bg.blit(flip_image, (0, self.height))
-
-        #----------------------------------------------------
-        # Запердолить в эту поверхность 2 фона
-        #----------------------------------------------------
-
-
-        self.speed = speed
-        self.y = 0
-        self.height = self.image.get_height()
+        image = pygame.image.load(image_path)
+        image_size = image.get_size()
+        self.height = image_size[1] * 2
+        self.bg = pygame.Surface((image_size[0], self.height))
+        self.bg.blit(source=image, dest=(0, 0))
+        self.bg.blit(
+            source=pygame.transform.flip(
+                surface=image,
+                flip_x=False,
+                flip_y=True,
+            ),
+            dest=(0, image_size[1]),
+        )
 
     def update(self):
         self.y += self.speed
         if self.y >= self.height:
             self.y = 0
 
-    def draw(self, screen):
-        screen.blit(self.image, (0, self.y))
-        screen.blit(self.image, (0, self.y - self.height))
+    def draw(self):
+        self.screen.blit(source=self.bg, dest=(0, self.y))
+        self.screen.blit(source=self.bg, dest=(0, self.y - self.height))
--- main.py
+++ main.py
@@ -13,12 +13,13 @@ import player_control
 
 
 pygame.init()
+
 screen = pygame.display.set_mode((420, 720))
 clock = pygame.time.Clock()
 running = True
 
 player = player_control.Player(screen, 200, 200)
-bg = background.Background()
+bg = background.Background(screen)
 
 while running:
 
@@ -26,7 +27,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     bg.update()
-    bg.draw(screen)
+    bg.draw()
     player.input()
     player.draw()
     pygame.display.flip()
