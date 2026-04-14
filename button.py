ca064fbf7aa5ad9d654060214ebe36d3074730fa
alex13132000 <bludniku1000@gmail.com>
2026-04-14 23:35:26 +0200

    button centre

--- button.py
+++ button.py
@@ -4,11 +4,12 @@ import pygame
 
 
 class Button:
-    def __init__ (self, scene, text, x, y, width, height):
+    def __init__ (self, scene, text, x, y, width, height, action=None):
         self.scene = scene
         self.text = text
         self.rect = pygame.Rect(x, y, width, height)
         self.font = pygame.font.SysFont('serif', 50)
+        self.action = action
         self.label = self.font.render(
             self.text,
             True,
@@ -17,7 +18,8 @@ class Button:
 
     def draw(self):
         pygame.draw.rect(self.scene.screen, 'black', self.rect)
-        self.scene.screen.blit(source=self.label, dest=self.rect)
+        text_rect = self.label.get_rect(center=self.rect.center)
+        self.scene.screen.blit(source=self.label, dest=text_rect)
 
     def is_clicked(self, event):
         return (
@@ -25,6 +27,10 @@ class Button:
             event.button == 1 and
             self.rect.collidepoint(event.pos)
         )
+    
+
 
     def on_click(self):
+        if self.action:
+            self.action()
         print(self.text)
