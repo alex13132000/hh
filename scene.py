65e732f062a599cf62fab051fd260af1775b4b23
alex13132000 <bludniku1000@gmail.com>
2026-04-17 19:34:28 +0200

    menu_2

--- scene.py
+++ scene.py
@@ -9,7 +9,6 @@ import hearts
 import player
 import score
 
-
 WIDTH, HEIGHT = 420, 720
 FPS = 60
 ENEMY_DELAY = 1.5
@@ -24,34 +23,14 @@ class Scene:
         pygame.mixer.music.load(BG_MUSIC)
         pygame.mixer.music.play(loops=-1)
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
-
-        self._background = background.Background(self)
         self._clock = pygame.time.Clock()
-        self._transients = []
-        self.hearts = hearts.Hearts(self)
-        self.player = player.Player(self, PLAYER_ZONE)
-        self.score = score.Score(self, SCORE_ZONE)
-        self.state = 'MENU'
-        self.last_enemy_timestamp = time.monotonic()
-        self.last_bullet_timestamp = time.monotonic()
+        self.resume_button = button.Button(self, 'Resume', 110, 200, 200, 60, self.action_resume)
+        self.restart_button = button.Button(self, 'Restart', 110, 300, 200, 60, self.action_restart)
+        self.exit_button = button.Button(self, 'Exit', 110, 400, 200, 60, self.action_exit)
         self.reset_game()
-        self.play_button = button.Button(
-            self,
-            'play',
-            110, 200, 200, 60, self.action_play
-            )
-        self.restart_button = button.Button(
-            self,
-            'restart',
-            110, 300, 200, 60, self.action_restart
-            )
-        self.exit_button = button.Button(
-            self,
-            'exit',
-            110, 400, 200, 60
-            )
+        self.state = 'MENU'
 
-    def action_play(self):
+    def action_resume(self):
         self.state = 'PLAY'
 
     def action_restart(self):
@@ -72,13 +51,14 @@ class Scene:
         if now - self.last_bullet_timestamp >= player.SHOT_DELAY:
             self._transients.append(bullet.Bullet(self, *self.player.rect.midtop))
             self.last_bullet_timestamp = now
-            self.player.shot_mp3.play()
+        self.player.shot_mp3.play()
 
     def get_bullets(self):
         return [o for o in self._transients if isinstance(o, bullet.Bullet)]
 
     def remove_transient(self, transient):
-        self._transients.remove(transient)
+        if transient in self._transients:
+            self._transients.remove(transient)
 
     def reset_game(self):
         self._background = background.Background(self)
@@ -109,13 +89,20 @@ class Scene:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.action_exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        if self.state == 'PLAY':
+                            self.state = 'MENU' 
+                        elif self.state == 'MENU':
+                            self.state = 'PLAY'
 
                 if self.state == 'MENU':
-                    if self.play_button.is_clicked(event):
-                        self.play_button.on_click()
+                    if self.resume_button.is_clicked(event):
+                        self.resume_button.on_click()
+                    if self.restart_button.is_clicked(event):
+                        self.restart_button.on_click()
                     if self.exit_button.is_clicked(event):
                         self.exit_button.on_click()
-
                 elif self.state == 'GAME_OVER':
                     if self.restart_button.is_clicked(event):
                         self.restart_button.on_click()
@@ -125,12 +112,11 @@ class Scene:
             if self.state == 'PLAY':
                 self.update()
                 self.draw()
-
             elif self.state == 'MENU':
                 self.screen.fill('blue')
-                self.play_button.draw()
+                self.resume_button.draw()
+                self.restart_button.draw()
                 self.exit_button.draw()
-
             elif self.state == 'GAME_OVER':
                 self.screen.fill('black')
                 self.restart_button.draw()
