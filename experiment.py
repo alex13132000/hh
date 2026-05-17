6ea629b31904cee2285a45bca157b199f21d37f4
alex13132000 <bludniku1000@gmail.com>
2026-05-17 16:24:21 +0200

    implement

--- experiment.py
+++ experiment.py
@@ -2,38 +2,6 @@ import sys
 
 import pygame
 
-# ENEMY_DATA = {
-#     "id": 1,
-#     "name": "Spiral Diver I",
-#     "tier": 1,
-#     "entry_path": {
-#         "type": "bezier",
-#         "control_points": [
-#             { "x": -0.2, "y": 1.1 },
-#             { "x": 0.1, "y": 0.8 },
-#             { "x": 0.3, "y": 0.5 },
-#             { "x": 0.5, "y": 0.2 }
-#         ]
-#     },
-#     "attack_path": {
-#         "type": "bezier",
-#         "control_points": [
-#             { "x": 0.5, "y": 0.2 },
-#             { "x": 0.45, "y": 0.4 },
-#             { "x": 0.5, "y": 0.7 },
-#             { "x": 0.5, "y": 1.1 }
-#         ]
-#     },
-#     "exit_path": {
-#         "type": "bezier",
-#         "control_points": [
-#             { "x": 0.5, "y": 1.1 },
-#             { "x": 0.7, "y": 1.2 },
-#             { "x": 1.1, "y": 1.0 },
-#             { "x": 1.1, "y": 1.0 },
-#         ]
-#     }
-# }
 
 ENEMY_DATA =     {
       "id": 6,
@@ -69,7 +37,7 @@ ENEMY_DATA =     {
     }
 
 
-WIDTH, HEIGHT = 1000, 1000
+WIDTH, HEIGHT = 800, 800
 FPS = 60  # frames per second
 SPEED = 100  # enemy speed pixels per second
 
